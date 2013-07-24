78a6c597c7d273a6c98601b33c910f2448b21053
bumbu <bmbalex@gmail.com>
2013-07-24 15:23:47 +0300

    add events and totals

--- app/js/application.js
+++ app/js/application.js
@@ -1,27 +1,34 @@
 $(function() {
-  var FormView, NoProductView, Product, ProductTracker, ProductView, Products, ProductsView;
+  var FormView, NoProductView, Product, ProductTracker, ProductView, Products, ProductsView, Totals, TotalsView;
   console.log('js loaded');
   ProductTracker = new Backbone.Marionette.Application();
   Product = Backbone.Model.extend({});
   Products = Backbone.Collection.extend({
     model: Product
   });
-  ProductView = Backbone.Marionette.ItemView.extend({
-    template: '#productView',
-    tagName: 'tr'
-  });
-  NoProductView = Backbone.Marionette.ItemView.extend({
-    template: '#noProductsView',
-    tagName: 'tr'
-  });
-  ProductsView = Backbone.Marionette.CompositeView.extend({
-    tagName: "table",
-    className: "table table-striped",
-    template: "#productsView",
-    itemView: ProductView,
-    emptyView: NoProductView,
-    appendHtml: function(collectionView, itemView) {
-      return collectionView.$("tbody").append(itemView.el);
+  Totals = Backbone.Model.extend({
+    defaults: {
+      average: 0,
+      total: 0,
+      count: 0
+    },
+    addValue: function(value) {
+      this.set({
+        'count': this.get('count') + 1,
+        'total': this.get('total') + 1.0 * value
+      });
+      return this.set({
+        'average': this.get('total') / this.get('count')
+      });
+    },
+    removeValue: function(value) {
+      this.set({
+        'count': this.get('count') - 1,
+        'total': this.get('total') - value
+      });
+      return this.set({
+        'average': this.get('total') / this.get('count')
+      });
     }
   });
   FormView = Backbone.Marionette.ItemView.extend({
@@ -38,21 +45,65 @@ $(function() {
         name: this.ui.name.val(),
         price: this.ui.price.val()
       });
+      ProductTracker.Totals.addValue(this.ui.price.val());
       this.ui.name.val('');
       return this.ui.price.val('');
     }
   });
+  ProductView = Backbone.Marionette.ItemView.extend({
+    template: '#productView',
+    tagName: 'tr',
+    events: {
+      'click button[data-action="remove"]': 'removeProduct',
+      'click button[data-action="edit"]': 'editProduct'
+    },
+    removeProduct: function() {
+      ProductTracker.Totals.removeValue(this.model.get('price'));
+      return this.model.destroy();
+    },
+    editProduct: function() {
+      return console.log('edit');
+    }
+  });
+  NoProductView = Backbone.Marionette.ItemView.extend({
+    template: '#noProductsView',
+    tagName: 'tr'
+  });
+  ProductsView = Backbone.Marionette.CompositeView.extend({
+    tagName: "table",
+    className: "table table-striped",
+    template: "#productsView",
+    itemView: ProductView,
+    emptyView: NoProductView,
+    appendHtml: function(collectionView, itemView) {
+      return collectionView.$("tbody").append(itemView.el);
+    }
+  });
+  TotalsView = Backbone.Marionette.ItemView.extend({
+    template: '#totalsView',
+    modelEvents: {
+      "change": "modelChanged"
+    },
+    modelChanged: function() {
+      return this.render();
+    }
+  });
   ProductTracker.addRegions({
     form: '#form',
-    list: '#list-table'
+    list: '#list-table',
+    totals: '#totals'
   });
   ProductTracker.addInitializer(function() {
-    ProductTracker.products = new Products();
+    ProductTracker.Products = new Products();
+    ProductTracker.Totals = new Totals();
     ProductTracker.form.show(new FormView({
-      collection: ProductTracker.products
+      collection: ProductTracker.Products
     }));
-    return ProductTracker.list.show(new ProductsView({
-      collection: ProductTracker.products
+    ProductTracker.list.show(new ProductsView({
+      collection: ProductTracker.Products
+    }));
+    return ProductTracker.totals.show(new TotalsView({
+      model: ProductTracker.Totals
     }));
   });
   return ProductTracker.start();
--- src/script/application.coffee
+++ src/script/application.coffee
@@ -6,10 +6,55 @@ $ ->
 	Product = Backbone.Model.extend {}
 	Products = Backbone.Collection.extend
 		model: Product
+	Totals = Backbone.Model.extend
+		defaults:
+			average: 0
+			total: 0
+			count: 0 # items count
+		addValue: (value)->
+			# @total += value
+			@.set
+				'count': @.get('count') + 1
+				'total': @.get('total') + 1.0 * value # TODO remove hack that forces value to be number
+			# Set average last as set uses a hash
+			@.set
+				'average': @.get('total') / @.get('count')
+		removeValue: (value)->
+			# @total -= value
+			@.set
+				'count': @.get('count') - 1
+				'total': @.get('total') - value
+			@.set
+				'average': @.get('total') / @.get('count')
+
+	FormView = Backbone.Marionette.ItemView.extend
+		template: '#formView'
+		events:
+			'click button': 'createNewProduct'
+		ui:
+			name: '#name'
+			price: '#price'
+		createNewProduct: ()->
+			@collection.add
+				name: @ui.name.val()
+				price: @ui.price.val()
+
+			ProductTracker.Totals.addValue @ui.price.val()
+
+			@ui.name.val ''
+			@ui.price.val ''
 
 	ProductView = Backbone.Marionette.ItemView.extend
 		template: '#productView'
 		tagName: 'tr'
+		events:
+			'click button[data-action="remove"]': 'removeProduct'
+			'click button[data-action="edit"]': 'editProduct'
+		removeProduct: ()->
+			ProductTracker.Totals.removeValue @model.get 'price'
+			@model.destroy()
+		editProduct: ()->
+			console.log 'edit'
 
 	NoProductView = Backbone.Marionette.ItemView.extend
 		template: '#noProductsView'
@@ -24,32 +69,28 @@ $ ->
 		appendHtml: (collectionView, itemView)->
 			collectionView.$("tbody").append itemView.el
 
+	TotalsView = Backbone.Marionette.ItemView.extend
+		template: '#totalsView'
+		modelEvents:
+			"change": "modelChanged"
+		modelChanged: ()->
+			@.render()
 
-	FormView = Backbone.Marionette.ItemView.extend
-		template: '#formView'
-		events:
-			'click button': 'createNewProduct'
-		ui:
-			name: '#name'
-			price: '#price'
-		createNewProduct: ()->
-			@collection.add
-				name: @ui.name.val()
-				price: @ui.price.val()
-
-			@ui.name.val ''
-			@ui.price.val ''
 
 	ProductTracker.addRegions
 		form: '#form'
 		list: '#list-table'
+		totals: '#totals'
 
 	ProductTracker.addInitializer ()->
-		ProductTracker.products = new Products()
+		ProductTracker.Products = new Products()
+		ProductTracker.Totals = new Totals()
 
 		ProductTracker.form.show new FormView
-			collection: ProductTracker.products
+			collection: ProductTracker.Products
 		ProductTracker.list.show new ProductsView
-			collection: ProductTracker.products
+			collection: ProductTracker.Products
+		ProductTracker.totals.show new TotalsView
+			model: ProductTracker.Totals
 
 	ProductTracker.start()
